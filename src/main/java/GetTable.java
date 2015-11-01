0a3786038a2932c281b71a0be2bf10a00b749f5b
pcontop <pcontopoulos@gmail.com>
2015-11-01 00:52:59 -0200

    Initial commit

---
+++ src/main/java/GetTable.java
@@ -0,0 +1,33 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.HTableInterface;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by pcont_000 on 01/11/2015.
+ */
+public class GetTable {
+    public static void main(String [] args){
+
+        Configuration conf = new Configuration();
+        try {
+            HTableInterface table = new HTable(conf, "excite");
+            Get g = new Get(Bytes.toBytes("hermes-201510301509"));
+            g.addColumn(Bytes.toBytes("colfam1"),null);
+            Result result = table.get(g);
+            List<Cell> values = result.listCells();
+            for (Cell c: values){
+                System.out.println("Valor Recuperado: " + Bytes.toString(c.getValueArray()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/test/java/TestConnect.java
@@ -0,0 +1,12 @@
+import org.apache.hadoop.hbase.client.HTableInterface;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by pcont_000 on 01/11/2015.
+ */
+public class TestConnect extends Assert {
+    @Test
+    public void testConnect(){
+    }
+}
